776feb4bb5a45907bea80669dd70888acdfb5087
Chris Clynes <chrisclynes@live.com>
2022-05-09 12:58:40 -0400

    updated searchbar, clear content working. App finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { MdSearch } from 'react-icons/md';
+import { MdSearch, MdOutlineClose } from 'react-icons/md';
 
 import  './Header.css';
 
+
+
 const Header = ({ setSearchValue }) => {
+    
+    const handleClearSearch = () => {
+        const search = document.getElementById("search-input-id");
+            search.value = search.defaultValue;
+            setSearchValue(search.value);
+    }
+
     return (
         <div className="app__header">
             <div className="app__title">
                 Notes
             </div>
             <div className="app__searchbar">
-                <MdSearch className="spyglass-search-icon" /><input type="text" className="search-input" placeholder="search..." onChange={(e) => setSearchValue(e.target.value)} />
+                <MdSearch className="spyglass-search-icon" /><input type="text" className="search-input" id="search-input-id" placeholder="search..." onChange={(e) => setSearchValue(e.target.value)} /><MdOutlineClose className="clear-search-icon" onClick={() => handleClearSearch()}/>
             </div>
         </div>
     )
